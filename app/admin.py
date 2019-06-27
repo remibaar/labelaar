3a9b799e1e87187c7bd88f475553dfc441a83622
Remi Baar <remibaar@gmail.com>
2019-06-27 17:21:01 +0200

    Create exporter of documents and create link in view
    Fixes #2

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from io import StringIO
+from django.http import HttpResponse
+from functools import update_wrapper
 
 from .models import Label, Document, DocumentImport
 from .forms import DocumentImportModelForm
-from .importer import import_file
+from .importer import import_file, export
 
 
 @admin.register(Label)
@@ -40,9 +43,36 @@ class DocumentAdmin(admin.ModelAdmin):
     list_display = ['shortened_text', 'annotated', 'document_import']
     search_fields = ['text']
     list_filter = ['annotated', 'labels__title', 'document_import']
+    change_list_template = 'admin/document_change_list.html'
 
     def get_actions(self, request):
         actions = super().get_actions(request)
         if 'delete_selected' in actions:
             del actions['delete_selected']
         return actions
+
+    def get_urls(self):
+        url_patterns = super().get_urls()
+        from django.urls import path
+
+        def wrap(view):
+            def wrapper(*args, **kwargs):
+                return self.admin_site.admin_view(view)(*args, **kwargs)
+            wrapper.model_admin = self
+            return update_wrapper(wrapper, view)
+
+        info = self.model._meta.app_label, self.model._meta.model_name
+
+        url_patterns = [
+            path('export/', wrap(self.export_view), name='%s_%s_export' % info)
+        ] + url_patterns
+
+        return url_patterns
+
+    def export_view(self, request, form_url='', extra_context=None):
+        bytes_io = StringIO()
+        export(bytes_io)
+        bytes_io.seek(0)
+        response = HttpResponse(bytes_io.read(), content_type="text/csv")
+        response['Content-Disposition'] = 'attachment; filename=labelaar_export.csv'
+        return response
--- app/importer.py
+++ app/importer.py
@@ -10,3 +10,20 @@ def import_file(document_import: DocumentImport, file, column, sep=','):
         document.text = text
         document.document_import = document_import
         document.save()
+
+
+def export(io_buffer):
+    documents = Document.objects.all()
+    records = []
+    columns = ['id', 'text', 'annotated', 'labels', 'label_ids']
+    for d in documents:
+        labels = d.labels.all()
+        records.append([
+            d.id,
+            d.text,
+            d.annotated,
+            ','.join([l.title for l in labels]),
+            ','.join([str(l.id) for l in labels]),
+        ])
+    df = pd.DataFrame(records, columns=columns)
+    df.to_csv(io_buffer, index=False)
